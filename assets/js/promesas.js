c3f628b1e9bfe5feb27442d96b8c63b8b9796657
lxishh <henrix913@gmail.com>
2024-05-28 12:35:02 -0400

    Tabla & get
    Se agregó tabla en index
    
    se agregó contenido en
    script/promesas

--- assets/js/promesas.js
+++ assets/js/promesas.js
@@ -1,8 +1,21 @@
-import {collection,addDoc} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {collection,addDoc, getDocs} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { db } from "./firebase.js";
 
 export const registrarPersona = async(persona)=>{
     console.log("persona")
     console.log(persona)
     const docRef = await addDoc(collection(db, "persona"), persona);
+}
+
+export const obtenerPersonas = async()=>{
+    const ref = collection(db,"persona");
+    const quearySnap = await getDocs(ref);
+    console.log(quearySnap);
+    let listado = []
+    quearySnap.forEach(doc => {
+        console.log(doc.id);
+        console.log(doc.data());
+        listado.push(doc.data());
+    });
+    return listado;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-import { registrarPersona } from "./promesas.js";
+import { obtenerPersonas, registrarPersona } from "./promesas.js";
 
 window.addEventListener("load",()=>{
     document.getElementById("btnRegistrar").addEventListener("click",registrar);
+    cargarDatos();
 });
 
 const registrar = ()=>{
@@ -22,8 +23,28 @@ const registrar = ()=>{
     console.log(objeto)
     registrarPersona(objeto).then(()=>{
         alert("Registrado con éxito")
+        cargarDatos();
     }).catch((r)=>{
         alert("Algo ocurrio")
         alert(r);
+    });
+}
+const cargarDatos = ()=>{
+    // Recuperar lo registrado en la base de datos
+    obtenerPersonas().then((personas)=>{
+        console.log("recuperado")
+        console.log(personas)
+        let estructura = "";
+        personas.forEach((persona)=>{
+            estructura += "<tr>";
+            estructura += "<td>" + persona.nombre + "<td>"
+            estructura += "<td>" + persona.apellido + "<td>"
+            estructura += "<td>" + persona.rut + "<td>"
+            estructura += "<td>" + persona.correo + "<td>"
+            estructura += "<td>" + persona.fnacimiento + "<td>"
+            estructura += "<td>" + persona.edad + "<td>"
+            estructura += "<tr>"
+        })
+        document.getElementById("tbDatos").innerHTML = estructura;
     });
 }
